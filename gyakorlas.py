e7aa5885988fee4c5c8090d609a707e25e36e73e
suliierik <82148730+suliierik@users.noreply.github.com>
2021-11-17 10:03:54 +0100

    Add files via upload

---
+++ gyakorlas.py
@@ -0,0 +1,12 @@
+list = ["Bea", "Tibor", "Dénes"]
+nev =input('Adj meg egy nevet:  ')
+if nev in list:
+    print('Benne van')
+else:
+    print('Nincs benne')
+
+
+#második feladat
+import random
+
+r = random.randrange(10,30)
